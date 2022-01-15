09e197ba3e34101d2e4835a7d17e88a318b67b3d
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-14 22:02:50 -0600

    Display stick figure parts when wrong letters are pressed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
 
-const figureParts = document.querySelectorAll('figure-part');
+const figureParts = document.querySelectorAll('.figure-part');
 
 const words = ['application','programming','interface', 'wizard'];
 
@@ -20,6 +20,18 @@ function updateWrongLettersEl() {
 		${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
 		${wrongLetters.map(letter => `<span>${letter}</span>`)}
 	`;
+	//not firing
+	figureParts.forEach((part, index) => {
+	// alert('test updateWrongLettersEl function');
+	
+		const errors = wrongLetters.length;
+
+		if(index < errors){
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
 }
 
 // Show notification
